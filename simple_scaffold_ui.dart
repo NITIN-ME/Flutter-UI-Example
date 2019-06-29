0f5511be06f5ec3286ff2c75c8e8851207748c6b
Nitin Gandhi <24195103+NITIN-ME@users.noreply.github.com>
2019-06-30 00:40:53 +0530

    Create simple_scaffold_ui.dart

---
+++ simple_scaffold_ui.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: 'Shopping App', home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar( title: Text('Flutter layout demo')),
+        body: Center( child: Text('Hello World')),
+      ),
+    );
+  }
+}
